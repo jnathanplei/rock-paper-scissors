e5d28eadb6cc668d24bc74932f373fb15b71fa6a
Nathan Lei <jnathanplei@gmail.com>
2022-06-18 01:18:58 -0700

    create a function playRound that plays a single round of Rock Paper Scissors

--- rps.js
+++ rps.js
@@ -1,10 +1,30 @@
 function computerPlay(max) {
     num = Math.floor(Math.random() * max);
     if (num === 0) {
-        return "Rock";
+        return "rock";
     } else if (num === 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == "rock" & computerSelection == "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection.toLowerCase() == "paper" & computerSelection == "scissors") {
+        return "You Lose! Scissors beat Paper";
+    } else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection.toLowerCase() == "rock" & computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection.toLowerCase() == "paper" & computerSelection == "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "paper") {
+        return "You Win! Scissors beats Paper";
+    }
+}
+
+const playerSelection = "paper";
+const computerSelection = computerPlay(3);
+console.log(playRound(playerSelection, computerSelection));
